e4c79d9489c0adfbde39a847700e2f7dac0ff9c7
rwheeler <rwheeler@rwheelers-MacBook-Pro.local>
2016-10-10 16:11:12 -0500

    simple working terminal (I think)

---
+++ WorkingTitle/Assets/TermCMD/Terminal.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic; 
+
+namespace Terminal{ 
+    public class VirtualFileSystem:Dictionary<string, Dictionary<string, object>>{
+    }
+    public class TerminalArt{
+        public static string initConsole(){
+        string cmd = @"   .-.
+                        (o.o)
+                        |=|
+                        __|__
+                    //.=|=.\\
+                    // .=|=. \\
+                    \\ .=|=. //
+                    \\(_=_)//
+                        (:| |:)
+                        || ||
+                        () ()
+                        || ||
+                        || ||
+                    l42 ==' '==
+                    SKULL -&- BONES OS
+                    V SPOOKY
+                    SYSTEM Init 2.0";
+    return cmd;
+        } 
+    }
+  public class Parser{
+        public static string termParse(string s){
+            //Parse commands for text based prompts and responses 
+            if (s.Contains("telnet")){
+                return @"CONNECTION='wired'
+                        DESCRIPTION='BroadMau'
+                        INTERFACE='eth0'
+                        IP='static'
+                        ADDR='10.10.10.5'
+                        GATEWAY='10.10.10.1'
+                        DNS='10.10.10.1'
+                        WPA_DRIVER='wext'
+                        NETCFG_DEBUG=yes
+
+                        connection: Faild
+                        ...";
+            }
+            else if (s.Equals("clear")){
+                return "";
+            }
+            else if (s.Equals("help")){
+                return @"telnet: access remote connections";
+                        
+            }
+
+            else{
+                return s;
+            }
+        }
+    }
+}
---
+++ WorkingTitle/Assets/TermInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine.UI;
+
+public class TermInput : MonoBehaviour {
+	InputField input; 
+	InputField.SubmitEvent se; 
+	public Text output;
+	Terminal.VirtualFileSystem vfs = new Terminal.VirtualFileSystem(); 
+
+	// Use this for initialization
+	void Start () {
+		input = gameObject.GetComponent<InputField>();
+		se = new InputField.SubmitEvent();
+		output.text = Terminal.TerminalArt.initConsole(); 
+		//Listener Callback 	
+		se.AddListener(SubmitInput);
+		input.onEndEdit = se;
+	}
+
+	private void SubmitInput(string arg0){
+		
+		string currentText = output.text;
+		string newText = (arg0.Equals("clear")) ? "" 
+			: currentText+ "\n" + Terminal.Parser.termParse(arg0);
+		output.text = newText;
+		input.text = "> ";
+		input.ActivateInputField(); 
+	}
+		
+}
