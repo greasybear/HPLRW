bb1bf2da73a9715211add6f4c41f70abf866126e
greasybear <captainfalconlink@hotmail.com>
2016-10-11 12:24:18 -0500

    Updated 10/11
    Now should be able to grab any rigidbody object to hold in front of
    camera. Still needs adjust but basic mechanic is there.

---
+++ WorkingTitle/Assets/PickUp.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{       private Transform pickItUp;
+
+        void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            transform.parent = pickItUp;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            transform.parent = null;
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+
+            pickItUp = col.transform;
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            pickItUp = null;
+        }
+
+    }
+}
