4b4809ace715ec9a156cbb7554aeff8d18382074
greasybear <captainfalconlink@hotmail.com>
2016-10-19 14:15:43 -0500

    Basic note reading
    Note reading system up, has gui text object, needs to be changed to a
    text reading function. Background also needs to be blurred. Additional
    Rooms are available for testing use and files are organized in the
    assets folders.

---
+++ WorkingTitle/Assets/Scripts/GamePlayScripts/ReadingNotes.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ReadingNotes : MonoBehaviour
+
+{
+
+
+    public GameObject noteReader;
+
+    public bool areYouReading;
+
+    private bool areYouClose;
+
+
+    void Start()
+    {
+        areYouReading = false;
+
+    }
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.transform.tag == "Player")
+            areYouClose = true;
+     }
+    void OnTriggerExit(Collider hit)
+    { if (hit.transform.tag == "Player")
+            areYouClose = false;
+                }
+
+
+
+    void Update()
+    {
+        if (areYouReading)
+        {
+            readerRabbit(true);
+        }
+        else {
+            readerRabbit(false);
+        }
+        if (Input.GetKeyDown(KeyCode.G) && areYouClose == true)
+        {
+            turnOnReader();
+        }
+
+    }
+    void readerRabbit(bool state)
+    {
+        if (state)
+        {
+            Time.timeScale = 0.0f; //reading a note
+        }
+        else {
+            Time.timeScale = 1.0f; //exiting reader
+            noteReader.SetActive(state);
+        }
+        noteReader.SetActive(state);
+    }
+    public void turnOnReader()
+    {
+        if (areYouReading)
+        {
+            areYouReading = false; //Changes the value
+        }
+        else
+        {
+            areYouReading = true;
+        }
+    }
+}
