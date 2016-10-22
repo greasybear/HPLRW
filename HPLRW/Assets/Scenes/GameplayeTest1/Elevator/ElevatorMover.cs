595430b3aed6acffa20c6461ad45861625e35740
greasybear <captainfalconlink@hotmail.com>
2016-10-21 21:22:25 -0500

    Updating Elevator-unfinished
    pushing changes to elevator, still needs work

---
+++ HPLRW/Assets/Scenes/GameplayeTest1/Elevator/ElevatorMover.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorMover : MonoBehaviour {
+
+    Animator Elevator;
+    Animator anim;
+    private bool inElevator;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.transform.tag == "Player")
+            inElevator = true;
+    }
+    void OnTriggerExit(Collider hit)
+    {
+        if (hit.transform.tag == "Player")
+            inElevator = false;
+    }
+
+
+
+
+        void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && inElevator == true)
+        {
+            anim.SetBool("GoingDown", true);
+            
+
+        }
+        if (Input.GetKeyUp(KeyCode.E) || inElevator == false)
+
+        {
+            anim.SetBool("GoingDown", false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.E) && inElevator == true)
+        {
+            anim.SetBool("GoingUp", true);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.E) || inElevator == false)
+
+        {
+            anim.SetBool("GoingUp", false);
+
+        }
+
+    }
+}
