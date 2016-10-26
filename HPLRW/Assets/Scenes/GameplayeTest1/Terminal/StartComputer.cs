bd32403c2c2738e97b1320bec4a96196b2203e07
greasybear <captainfalconlink@hotmail.com>
2016-10-26 10:03:45 -0500

    Added terminal camera

---
+++ HPLRW/Assets/Scenes/GameplayeTest1/Terminal/StartComputer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartComputer : MonoBehaviour {
+    public GameObject objectToEnable;
+    public GameObject objectToDisable;
+    public bool onOff;
+    public bool areYouClose;
+
+	
+	void Start () {
+        onOff = false;
+        areYouClose = false;
+	
+	}
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.transform.tag == "Player")
+            areYouClose = true;
+    }
+    void OnTriggerExit(Collider hit)
+    {
+        if (hit.transform.tag == "Player")
+            areYouClose = false;
+    }
+
+    void Update () {
+        if (Input.GetButtonDown("Cancel") && areYouClose==true)
+            computerOn();
+        if (onOff == true)
+        { objectToDisable.SetActive(false);
+            objectToEnable.SetActive(true);}
+        else
+        { objectToDisable.SetActive(true);
+            objectToEnable.SetActive(false);}
+	}
+    public void computerOn()
+    { if (onOff)
+        { onOff = false; }
+        else { onOff = true; }
+    }
+    
+
+
+}
